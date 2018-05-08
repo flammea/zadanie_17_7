f949216cea74b85e403b24f6182f6f77e65f89ae
Beata Markunas <beata.markunas@gmail.com>
2018-05-08 06:13:34 +0200

    Create config file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '134256913719-jah43s9bcs60tqjm3jjd1depe91e0o00.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'jJAWnjAxy3d-ci0P9YUL0H_m',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
